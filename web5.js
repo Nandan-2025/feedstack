bd186585bbd9bbd9902e8b07bb632251cf15586b
Nandan-2025 <nandanv.v@s.amity.edu>
2025-05-20 10:07:31 +0530

    5 reset and body

---
+++ web5.js
@@ -0,0 +1,14 @@
+##  reset and body.
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f2f3f7;
+  color: #333;
+}
+
