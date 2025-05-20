4df7d670adbfb8072cdc124a788a2b2d4bc72735
Nandan-2025 <nandanv.v@s.amity.edu>
2025-05-20 10:15:07 +0530

    10 scripts.js

---
+++ web10.js
@@ -0,0 +1,13 @@
+##  script.js
+
+document.getElementById("username").innerText = "Jane Smith";
+document.getElementById("followers").innerText = "250";
+document.getElementById("following").innerText = "300";
+document.getElementById("posts").innerText = "42";
+
+const postList = document.getElementById("postList");
+const newPost = document.createElement("li");
+newPost.textContent = "New blog post coming soon!";
+postList.appendChild(newPost);
+
+
