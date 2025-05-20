9cb4b3a848c4672d3a2e98c14c90dd6a1f172622
Nandan-2025 <nandanv.v@s.amity.edu>
2025-05-20 10:09:31 +0530

    6 container and header

---
+++ web6.js
@@ -0,0 +1,17 @@
+33 container and header.
+
+.container {
+  width: 90%;
+  max-width: 600px;
+  margin: 20px auto;
+  background: #fff;
+  padding: 20px;
+  border-radius: 12px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+header h1 {
+  text-align: center;
+  color: #1da1f2;
+}
+
