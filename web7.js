0bb03332bd0dcbfca80d57b5aee770cfbe49827d
Nandan-2025 <nandanv.v@s.amity.edu>
2025-05-20 10:11:43 +0530

    7 profile section

---
+++ web7.js
@@ -0,0 +1,20 @@
+33 profile section. 
+
+
+.profile {
+  text-align: center;
+  margin-top: 20px;
+}
+
+.profile-pic {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+}
+
+.bio {
+  font-style: italic;
+  margin: 10px 0;
+}
+
+
