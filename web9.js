b288a729b8a84acb39b6072c3b57130506ce1540
Nandan-2025 <nandanv.v@s.amity.edu>
2025-05-20 10:14:05 +0530

    9 post section

---
+++ web9.js
@@ -0,0 +1,18 @@
+##  posts section .
+
+
+.posts {
+  margin-top: 20px;
+}
+
+.posts ul {
+  list-style-type: none;
+}
+
+.posts li {
+  background-color: #e8f5fd;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 8px;
+}
+
